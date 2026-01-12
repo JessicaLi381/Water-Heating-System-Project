e0e2837894f49170930d41599059ad0d55975f13
juiicie <li.jessica381@gmail.com>
2026-01-11 23:43:36 -0500

    calculated heat retention

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
-data = {
-    "Material": ["Towel", "BubbleWrap", "Foam", "Cardboard", "FoilAirGap", "Thermos"], 
-    "Cost": [2.00, 1.50, 3.00, 0.75, 1.00, 15.00]
-}
+#constants
+C_WATER = 4.189 #j/g°C
+MASS_WATER = 1000.0 #g
+T_INITIAL = 25.0 #°C
 
-df = pd.DataFrame(data, index=["a", "b", "c", "d", "e", "f"])
+#csv data
+df = pd.read_csv('data.csv')
 
-# add new column
-df["initial_temp"] = [60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
-
-# add new row 
-new_row = pd.DataFrame([{"Material": "NewMaterial", "Cost": 5.00, "initial_temp": 60.0}], index=["g"])
-df = pd.concat([df, new_row])
-
-# add new rows
-new_rows = pd.DataFrame([{"Material": "NewMaterial1", "Cost": 5.00, "initial_temp": 60.0},
-                         {"Material": "NewMaterial2", "Cost": 6.00, "initial_temp": 60.0}])
-df = pd.concat([df, new_rows])
+#calculate heat retained and add as new column (q = mcΔT)
+df['heat_retained_j'] = MASS_WATER * C_WATER * (df['temp']- T_INITIAL)
 
 print(df)
